434bd0b142bbccae075da66654d45493ea2f3a4b
Andrew S <>
2025-05-14 15:26:20 +0300

    Migrate from HttpClient4 to HttpClient5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,42 +1,39 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.HttpHeaders;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.message.BasicHeader;
+
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
     public static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+    public static void main(String[] args) throws IOException, ParseException {
+        CloseableHttpClient httpClient = HttpClients.custom()
                 .setUserAgent("My Test Service")
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)
-                        .setSocketTimeout(30000)
-                        .setRedirectsEnabled(false)
-                        .build())
+                .setDefaultHeaders(List.of(new BasicHeader(HttpHeaders.ACCEPT, "application/json")))
                 .build();
 
         HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
-        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
 
         CloseableHttpResponse response = httpClient.execute(request);
 
         List<Fact> facts = mapper.readValue(
-                response.getEntity().getContent(),
-                new TypeReference<List<Fact>>() {}
-        ).stream()
+                        EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8),
+                        new TypeReference<List<Fact>>() {}
+                ).stream()
                 .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
-                .collect(Collectors.toList());
+                .toList();
         facts.forEach(System.out::println);
 
         response.close();
